bd2bbb1d9e2790b2bf7454f9af8272b172de3d5a
winkt0 <238452092+winkt0@users.noreply.github.com>
2025-12-08 22:28:26 +0100

    formatting function for folder size

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,40 @@ impl PartialEq for ProgramFolder {
     }
 }
 
+struct Unit<'a> {
+    size: u64,
+    name: &'a str,
+}
+
+const GB: Unit = Unit {
+    size: 1 << 30,
+    name: "GB",
+};
+
+const MB: Unit = Unit {
+    size: 1 << 20,
+    name: "MB",
+};
+
+const KB: Unit = Unit {
+    size: 1 << 10,
+    name: "KB",
+};
+
+fn format_size(bytes: u64) -> String {
+    match [GB, MB, KB].iter().find(|unit| bytes > unit.size) {
+        Some(unit) => {
+            let num_units = bytes / unit.size;
+            return num_units.to_string()
+                + " "
+                + &unit.name
+                + " "
+                + &format_size(bytes - num_units * unit.size);
+        }
+        None => return bytes.to_string() + " bytes",
+    };
+}
+
 fn main() {
     let limit = 20;
     let mut found = BTreeSet::<ProgramFolder>::new();
@@ -102,6 +136,6 @@ fn main() {
         .unwrap();
     find_biggest_programs(&path, &mut found, limit);
     for folder in found {
-        println!("{} with {} Bytes", folder.path, folder.bytes);
+        println!("{} with {}", folder.path, format_size(folder.bytes));
     }
 }
